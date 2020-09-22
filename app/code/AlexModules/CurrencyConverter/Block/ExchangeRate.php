14583e60c85c53242d803f07bfec59873879c4a5
Alex Kovalchuk <sanyol255@gmail.com>
2020-09-22 19:25:05 +0300

    Creating Currency Converter module with 2 static columns

---
+++ app/code/AlexModules/CurrencyConverter/Block/ExchangeRate.php
@@ -0,0 +1,21 @@
+<?php
+namespace AlexModules\CurrencyConverter\Block;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use AlexModules\CurrencyConverter\Model\CurrencyData;
+
+class ExchangeRate extends Template
+{
+    private $currencyData;
+
+    public function __construct(Context $context, CurrencyData $currencyData, array $data = [])
+    {
+        $this->currencyData = $currencyData;
+        parent::__construct($context, $data);
+    }
+
+    public function getCurrency()
+    {
+        return $this->currencyData;
+    }
+}
---
+++ app/code/AlexModules/CurrencyConverter/Controller/Converter/Index.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AlexModules\CurrencyConverter\Controller\Converter;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
