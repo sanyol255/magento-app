4d639c3f09d745c849d93c34828dd7c66e76cee4
Alex Kovalchuk <sanyol255@gmail.com>
2020-10-06 19:43:32 +0300

    Refactoring FormatPrice class for changing price precision

--- app/code/AlexModules/PricePrecision/Locale/FormatPrice.php
+++ app/code/AlexModules/PricePrecision/Locale/FormatPrice.php
@@ -7,56 +7,9 @@ class FormatPrice extends Format
 {
     public function getPriceFormat($localeCode = null, $currencyCode = null)
     {
-        $localeCode = $localeCode ?: $this->_localeResolver->getLocale();
-        if ($currencyCode) {
-            $currency = $this->currencyFactory->create()->load($currencyCode);
-        } else {
-            $currency = $this->_scopeResolver->getScope()->getCurrentCurrency();
-        }
-
-        $formatter = new \NumberFormatter(
-            $currency->getCode() ? $localeCode . '@currency=' . $currency->getCode() : $localeCode,
-            \NumberFormatter::CURRENCY
-        );
-        $format = $formatter->getPattern();
-        $decimalSymbol = $formatter->getSymbol(\NumberFormatter::DECIMAL_SEPARATOR_SYMBOL);
-        $groupSymbol = $formatter->getSymbol(\NumberFormatter::GROUPING_SEPARATOR_SYMBOL);
-
-        $pos = strpos($format, ';');
-        if ($pos !== false) {
-            $format = substr($format, 0, $pos);
-        }
-        $format = preg_replace("/[^0\#\.,]/", '', $format);
-        $totalPrecision = 0;
-        $decimalPoint = strpos($format, '.');
-        if ($decimalPoint !== false) {
-            $totalPrecision = strlen($format) - (strrpos($format, '.') + 1);
-        } else {
-            $decimalPoint = strlen($format);
-        }
-        $requiredPrecision = $totalPrecision;
-        $t = substr($format, $decimalPoint);
-        $pos = strpos($t, '#');
-        if ($pos !== false) {
-            $requiredPrecision = strlen($t) - $pos - $totalPrecision;
-        }
-
-        if (strrpos($format, ',') !== false) {
-            $group = $decimalPoint - strrpos($format, ',') - 1;
-        } else {
-            $group = strrpos($format, '.');
-        }
-
-        $result = [
-            'pattern' => $currency->getOutputFormat(),
-            'precision' => 1,
-            'requiredPrecision' => 1,
-            'decimalSymbol' => $decimalSymbol,
-            'groupSymbol' => $groupSymbol,
-            'groupLength' => $group,
-            'integerRequired' => $totalPrecision == 0,
-        ];
-
+        $result = parent::getPriceFormat($localeCode, $currencyCode);
+        $result['precision'] = 1;
+        $result['requiredPrecision'] = 1;
         return $result;
     }
 }
